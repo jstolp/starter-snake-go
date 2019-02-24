84025ad74f86897a093ec27d9a5e3e6fec31c0fe
Jeroen Stolp <jeroen.stolp@flexkids.nl>
2019-02-24 13:16:26 +0100

    correct formula for biggest edge snake

--- routes.go
+++ routes.go
@@ -11,63 +11,81 @@ import (
 	"strconv"
 )
 
-var topBound int = 0
-var leftBound int = 0
-var rightBound int = 0
-var botBound int = 0
+var leftBound int = 1;
+var topBound int = 1;
+var rightBound int = 0;
+var botBound int = 0;
 
-var turn int = 0
-var move string = "down"
-var nextMove string = ""
-var prevMove string = ""
-var headPos Coord
-var tailPos Coord
-
-
-func dd(obj interface{}) {
-	data, err := json.MarshalIndent(obj, "", "  ")
-	if err == nil {
-		log.Printf(string(data))
-	}
-}
-
-func getTailPos(target Snake) Coord {
-	body := target.Body
-  return body[len(body) - 1]
-}
-
-func getHeadPos(target Snake) Coord {
-	body := target.Body
-  return body[0]
-}
+var edgeSnakeLimit int = 0;
+var turn int = 0;
+var move string = "down";
+var nextMove string = "";
+var prevMove string = "";
+var headPos Coord;
+var tailPos Coord;
+var health int = 100;
+var numOfStartingSnakes int = 1;
+var numSnakesLeft int = 1;
+var enemySnakes int = 0;
+var foodPointList []Coord;
+var endCicle bool = false;
 
 /* heads: "beluga" "bendr" "dead" "evil" "fang" "pixel" "regular" "safe" "sand-worm" "shades" "silly" "smile" "tongue"
 tails: "block-bum" "bolt" "curled" "fat-rattle" "freckled" "hook" "pixel" "regular" "round-bum" "sharp" "skinny" "small-rattle" */
 
+func shrinkArena() {
+	leftBound = leftBound + 1
+	topBound = topBound + 1
+	rightBound = rightBound - 1
+	botBound = botBound - 1
+	// edgeSnakeLimit = ((botBound - 1) * (rightBound - 1)) - FALSE ASSUMPTION. it doesn't work if you shrink, because you are bigger
+
+	log.Print("BOARD Size: TOP LEFT  NW Corner x:" + strconv.Itoa(topBound) + " , " + strconv.Itoa(leftBound))
+	log.Print("BOARD Size: BOT RIGHT SE Corner x:" + strconv.Itoa(botBound) + "," + strconv.Itoa(rightBound))
+	log.Println("Snake Edge Limit: " + strconv.Itoa(edgeSnakeLimit))
+
+	log.Print("Shrunk the Area by 1x1... new SIZES \n")
+}
+
 func Start(res http.ResponseWriter, req *http.Request) {
-
-
-
 	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Lmicroseconds)
-	log.Print("START: start")
 	decoded := SnakeRequest{}
 	err := DecodeSnakeRequest(req, &decoded)
 	if err != nil {
 		log.Printf("Bad start request: %v", err)
 	}
 
-	rightBound = decoded.Board.Width
-	botBound = decoded.Board.Height
+  headPos = decoded.You.Body[0]
+  //foodPointList := decoded.Board.Food
+  numOfStartingSnakes = len(decoded.Board.Snakes)
+  topBound, leftBound = 1, 1; // Set NW bound, X, Y
+	botBound, rightBound = decoded.Board.Height, decoded.Board.Width // SE corner X, Y
 
-	log.Print("BOARD: top: " + strconv.Itoa(topBound) + " bot: " + strconv.Itoa(botBound) + "left: " + strconv.Itoa(leftBound) + " right " + strconv.Itoa(rightBound))
+	edgeSnakeLimit = (((botBound - 1) * 2) + ((rightBound - 1) * 2))
 
+	log.Print("BOARD Size: TOP LEFT  NW Corner x:" + strconv.Itoa(topBound) + " , " + strconv.Itoa(leftBound))
+	log.Print("BOARD Size: BOT RIGHT SE Corner x:" + strconv.Itoa(botBound) + "," + strconv.Itoa(rightBound))
+	log.Println("Snake Edge Limit: " + strconv.Itoa(edgeSnakeLimit))
+	log.Print("Enemy Snakes: " + strconv.Itoa(numOfStartingSnakes - 1) + "\n\n")
+
+	log.Print("Start Pos: " + strconv.Itoa(headPos.X) + "," + strconv.Itoa(headPos.Y))
+	if(numOfStartingSnakes == 1) {
+		log.Print("\n\n It's Gonna be a SOLO GAME \n")
+	}
+	/*
+	 e19c41 - orange test 2
+   00ff55 - green
+   ff4f00 - orange test 1 -nee te rood
+	*/
 	respond(res, StartResponse{
-		Color: "#00ff55",
-		HeadType: "fang",
-		TailType: "bolt",
+		Color: "#e19c41",
+		HeadType: "tongue",
+		TailType: "curled",
 	})
 }
 
+// Check if MoveIs Out of Bounds...
+// What a horror function.... v0.2.0 consider refactor
 func isMoveOOB(headPos Coord, direction string) bool {
 	switch direction {
 		case "down":
@@ -75,11 +93,11 @@ func isMoveOOB(headPos Coord, direction string) bool {
 					return false
 			}
 		case "up":
-			if (headPos.Y > topBound) {
+			if (headPos.Y + 1 > topBound) {
 				return false
 			}
 		case "left":
-			if(headPos.X > leftBound) {
+			if(headPos.X + 1 > leftBound) {
 				return false
 			}
 		case "right":
@@ -87,7 +105,18 @@ func isMoveOOB(headPos Coord, direction string) bool {
 				return false
 			}
 	}
-return true
+	return true
+}
+
+// closestFoodPoint
+func minDistFood(headPos Coord, food []Coord) Coord {
+	min := food[0]
+	for _, f := range food {
+		if dist(min, headPos) < dist(f, headPos) {
+			min = f
+		}
+	}
+	return min
 }
 
 func randomNOOBmove(headPos Coord, currentDir string) string {
@@ -113,23 +142,127 @@ func Move(res http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		log.Printf("Bad move request: %v", err)
 	}
-	turn = decoded.Turn
-	log.Print("TURN " + strconv.Itoa(turn) + "\n")
 
-	headPos := getHeadPos(decoded.You)
-	nextMoveOOB := isMoveOOB(headPos, nextMove)
-	if (nextMoveOOB) {
-		nextMove = randomNOOBmove(headPos, move)
+	me := decoded.You
+	foodPointList = decoded.Board.Food
+	health = me.Health
+	myLength := len(me.Body)
+	numSnakesLeft = len(decoded.Board.Snakes)
+	enemySnakes = numSnakesLeft - 1
+	turn = decoded.Turn
+
+/*
+	if (me.Body[0].X == 0 && me.Body[0].Y == 0 && myLength == edgeSnakeLimit) {
+		log.Print("IM TOP LEFT... \n\n")
+		shrinkArena()
+	} else {
+		dd(me.Body[0])
+	}
+*/
+	if (health == 1) {
+		log.Print("Dag Mooie Wereld... Hongersnood is geen grapje... \n\n")
 	}
 
+	if (myLength == edgeSnakeLimit) {
+			log.Println("circle JErk")
+	}
+
+	if (myLength > edgeSnakeLimit) {
+			log.Println("DEATH DEATH DEATH TAILCRASH")
+	}
+	if (enemySnakes < 1) {
+		//log.Print("TURN " + strconv.Itoa(turn))
+		log.Print("SOLO " + strconv.Itoa(turn) + "MY LENGTH: " + strconv.Itoa(myLength) +" h: "+ strconv.Itoa(health) + "\n")
+	} else {
+		log.Print("TURN " + strconv.Itoa(turn) + " e: "+ strconv.Itoa(enemySnakes)+" h: "+ strconv.Itoa(health) + "\n")
+	}
+
+	headPos := getHeadPos(decoded.You)
+	nextMoveIsOOB := isMoveOOB(headPos, nextMove)
+	if (nextMoveIsOOB) {
+		// CLOCKWISE: invDir(randomNOOBmove(headPos, move))
+		nextMove = randomNOOBmove(headPos, move)
+		// COUNTER-CLOCKWISE: randomNOOBmove(headPos, move)
+	}
+
+	if (endCicle) {
+		endCicle = false;
+		log.Print("END CIRCLE COMMAND. SHRuhk the arena... i should VEERE... \n")
+	}
+	// if im bigger... i can't do the edge snake strategy...
+  if (endCicle == false && myLength == edgeSnakeLimit) {
+			log.Println("CirleJerk... Infinity SNAKEEE... let's switch the strat.")
+			shrinkArena()
+			nextMove = randomNOOBmove(headPos, move)
+			//nextMove = randomNOOBmove(headPos, move)
+	}
+/*
+	if (health < 30) {
+		closestFoodPoint := minDistFood(headPos,foodPointList)
+		dd(closestFoodPoint)
+		log.Print("Im going to die of starvation in " + strconv.Itoa(health) + " turns \n\n")
+		foodDir := goToDir(headPos,closestFoodPoint)
+
+		if(!isMoveOOB(headPos, foodDir)) {
+				nextMove = foodDir
+		} else {
+				nextMove = randomNOOBmove(headPos, move)
+		}
+		if (isNextMoveFatal(headPos, prevMove, nextMove)) {
+			// last ditch effort to correct...
+			nextMove = invDir(nextMove)
+		}
+	} // end HEALTH LOW
+*/
+
+  test := isNextMoveFatal(me, prevMove, nextMove)
 	move = nextMove // finalise the move
-	fmt.Print("Move: " + move)
+	fmt.Print(strconv.Itoa(turn) + "Move: " + nextMove + "\n Is fatal: ")
+	fmt.Print(test)
 	fmt.Println()
 	respond(res, MoveResponse{
-		Move: move,
+		Move: nextMove,
 	})
-	prevMove = move // Re-allocate move command to prev/last move\
+	prevMove = nextMove // Re-allocate move command to prev/last move\
 }
+
+
+func isNextMoveFatal(me Snake, currentDir string, targetDir string) bool {
+		// doing a 180 is never safe, so check for that...
+		flipDir := invDir(currentDir)
+		if(flipDir == targetDir) {
+			log.Print("The move is " + targetDir + "but in going " + currentDir + "That would be fatal...\n")
+			return true
+		}
+		// check if a move is NOT_OUT_OF_BOUNDS (hit a wall) WALL SNAKE
+		if (isMoveOOB(headPos, targetDir)) {
+			log.Print("Next Move is Fatal because of a BOUNDARY " + targetDir + "\n")
+			return true
+		}
+
+		// if dist to my own tail is 1, and i'm going in the same direction...
+		// i'll die...
+		if (dist(headPos, tailPos) == 1 && targetDir == goToDir(headPos, tailPos)) {
+			log.Print("CRASHING INTO MY OWN TAIL IN ... 3 . 2.. .1.. no... next MOVE ahhaah \n\n")
+			log.Print()
+			return true
+		}
+		log.Print("The move " + targetDir + " is safe...\n")
+		return false
+}
+
+
+// see if i can attach these methods to the struct Snake or something..
+func getHeadPos(target Snake) Coord {
+	body := target.Body
+  return body[0]
+}
+func getTailPos(target Snake) Coord {
+	body := target.Body
+  return body[len(body) - 1]
+}
+
+
 /*
 var prevDir := "na"
 var currentDir := "na"
@@ -141,8 +274,10 @@ func dist(a Coord, b Coord) int {
 	return int(math.Abs(float64(b.X-a.X)) + math.Abs(float64(b.Y-a.Y)))
 }
 
+
+
 /* move from coord to coord -> returns MOVE */
-func GoToDir(curr Coord, next Coord) string {
+func goToDir(curr Coord, next Coord) string {
 	dir := ""
 	if curr.X < next.X {
 		dir = "right"
@@ -179,6 +314,15 @@ func invDir(currentDir string) string {
 		return dir
 }
 
+// just a testing function to dump a object../
+func dd(obj interface{}) {
+	data, err := json.MarshalIndent(obj, "", "  ")
+	if err == nil {
+		log.Printf(string(data))
+	}
+}
+
+// Extra route
 func Index(res http.ResponseWriter, req *http.Request) {
 	/* Battlesnake documentation can be found at <a href=\"https://docs.battlesnake.io\">https://docs.battlesnake.io</a>. */
 	configuration := Configuration{}
@@ -187,7 +331,7 @@ func Index(res http.ResponseWriter, req *http.Request) {
 		log.Printf("Bad configuration in config.json: %v", errConf)
 	}
 	res.WriteHeader(http.StatusOK)
-	res.Write([]byte("Jay's battleSnake mk 1 self aware: " + configuration.Home_Route))
+	res.Write([]byte("Jay's battleSnake mk III self aware: " + configuration.Home_Route))
 }
 
 func Ping(res http.ResponseWriter, req *http.Request) {
